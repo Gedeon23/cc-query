296a72466cd3b5c13afa8b6d5fa5a237927c605c
Gedeon Jahns <gedeon.jahns@gmail.com>
2024-03-11 03:14:53 +0100

    some improvements to the log server

--- log_server.lua
+++ log_server.lua
@@ -2,7 +2,8 @@ local modem = rednet.open("top")
 local monitor = peripheral.find("monitor")
 local cursor_y = 1
 local names = {}
-
+local terminal_width, terminal_height = monitor.getSize()
+local number_of_rows = terminal_height * 6 - 2
 
 monitor.clear()
 monitor.setCursorPos(1, 1)
@@ -10,9 +11,9 @@ monitor.setCursorPos(1, 1)
 
 
 while true do
-    local id, message = rednet.receive()
+    local id, message = rednet.receive("log")
     monitor.write(id..": "..message)
-    if cursor_y < 9 then
+    if cursor_y < number_of_rows then
         cursor_y = cursor_y + 1
     else
         monitor.scroll(1)
