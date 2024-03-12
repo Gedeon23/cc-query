44aa26ce0dff4a8b4cdec58e5a499eaace7c507a
Gedeon Jahns <gedeon.jahns@gmail.com>
2024-03-12 19:54:44 +0100

    better log

--- log.lua
+++ log.lua
@@ -1,6 +1,9 @@
-function log(message)
+function log(...)
+    local args = {...}
     local logfile = "/log.txt"
     local file
+
+    message = listToString(args)
 
     if fs.exists(logfile) then
         file = fs.open(logfile, "a")
@@ -12,6 +15,37 @@ function log(message)
     rednet.broadcast(message, "log")
     file.writeLine(message)
     file.close()
+end
+
+function listToString(list)
+    result = ""
+    seperator = " "
+    for i, v in pairs(list) do
+        if type(v) == "table" then
+            results = result..seperator..tableToString(v)
+        else
+            result = result..seperator..v
+        end
+    end
+
+    return result
+end
+
+
+function tableToString(tbl)
+    local result = "{"
+    local seperator = ""
+    for k, v in pairs(tbl) do
+        result = result..seperator..k..": "..
+        if type(v) == "table" then
+            result = result..tableToString(v)
+        else
+            result = result..v
+        end
+        seperator = ", "
+    end
+    result = result.."}"
+    return result
 end
 
 return log
