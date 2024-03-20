88311220a88d905cef2454883ce86c9131171b64
Gedeon Jahns <gedeon.jahns@gmail.com>
2024-03-21 00:17:18 +0100

    better item datastructure

--- programs/inv.lua
+++ programs/inv.lua
@@ -1,14 +1,27 @@
-log = require("log")
-
 inv = {
     chests = { peripheral.find("minecraft:chest") },
-    barrels = { peripheral.find("minecraft:barrels") },
+    barrels = { peripheral.find("minecraft:barrel") },
     items = {}
 }
 
 function inv:takeStock()
-    self.items = {}
-    for index, barrel in pairs(self.barrels) do
-        log(barrel.list())
-    end
 end
+
+function inv:getItems()
+    self.items = {}
+    for bIndex, barrel in pairs(self.barrels) do
+        for sIndex, slot in pairs(barrel.list()) do
+            if self.items[slot.name] == nil then
+                self.items[slot.name] = {count = slot.count}
+                self.items[slot.name].location = {bIndex = {sIndex}}
+            else
+                self.items[slot.name].count = self.items[slot.name] + slot.count
+                table.insert(self.items[slot.name].location[bIndex], sIndex)
+            end
+        end
+    end
+
+    return self.items
+end
+
+print(textutils.serialise(inv:getItems()))
