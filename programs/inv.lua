997aefa6e8365f48a88e09bd0e5b4d0ae895825f
Gedeon Jahns <gedeon.jahns@gmail.com>
2024-03-27 00:29:00 +0100

    integrated ui into inv

--- programs/inv.lua
+++ programs/inv.lua
@@ -4,13 +4,27 @@ inv = {
     chests = { peripheral.find("minecraft:chest") },
     barrels = { peripheral.find("minecraft:barrel") },
     items = {}
+    ui = {}
 }
+
+function inv:buildUI()
+    self.ui.main = basalt.createFrame()
+    self.ui.flex = main:addFlexbox():setWrap("wrap"):setPosition(1,1):setSize("parent.w", "parent.h")
+    self.ui.leftColumn = flex:addFlexbox():setDirection("column"):setSpacing(0)
+    self.ui.rightColumn = flex:addFlexbox():setDirection("column")
+    self.ui.itemSearch = leftColumn:addInput():setInputType("text"):onChar(function(self, event, char)
+        inv:updateItemList()
+    )
+    self.ui.itemList = leftColumn:addList()
+    self.ui.itemName = rightColumn:addLabel():setText("placeholder"):setFontSize(1)
+
+    basalt.autoUpdate()
+end
 
 function prettifyName(name)
     local beginning = string.find(name, ":") + 1
     return name:sub(beginning, string.len(name)):gsub("_", " ")
 end
-
 
 function inv:takeStock()
     self.items = {}
@@ -52,26 +66,6 @@ function inv:printItemList()
     end
 end
 
-local main = basalt.createFrame()
 
 inv:takeStock()
-
-local flex = main:addFlexbox():setWrap("wrap"):setPosition(1,1):setSize("parent.w", "parent.h")
-local leftColumn = flex:addFlexbox():setDirection("column"):setSpacing(0)
-local rightColumn = flex:addFlexbox():setDirection("column")
-local itemSearch = leftColumn:addInput():setInputType("text")
-local itemList = leftColumn:addList()
-local itemName = rightColumn:addLabel()
-itemName:setText("placeholder")
-itemName:setFontSize(1)
-
-for _, item in pairs(inv.items) do
-    itemList:addItem(item.name.." "..item.count, colors.lightGray, colors.black, item)
-end
-
-itemList:onSelect(function(self, event, item)
-    itemName:setText(textutils.serialise(item.args.location))
-    basalt.debug(textutils.serialise(item))
-end)
-
-basalt.autoUpdate()
+inv:buildUI()
